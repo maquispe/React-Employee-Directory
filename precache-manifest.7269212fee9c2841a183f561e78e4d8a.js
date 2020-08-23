35e9b1db809b7d4af1566ce67495f56034420e7f
maquispe <62685159+maquispe@users.noreply.github.com>
2020-08-23 00:22:06 -0700

    Updates

---
+++ precache-manifest.7269212fee9c2841a183f561e78e4d8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae7b4de1c071968980534ef1bb04269",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "eb629d69e34dc76a378c",
+    "url": "/React-Employee-Directory/static/css/main.1a64f172.chunk.css"
+  },
+  {
+    "revision": "446c1a65ce88b6e710c7",
+    "url": "/React-Employee-Directory/static/js/2.ea430d09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Employee-Directory/static/js/2.ea430d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb629d69e34dc76a378c",
+    "url": "/React-Employee-Directory/static/js/main.bf01958c.chunk.js"
+  },
+  {
+    "revision": "790a6e69fd277117afd5",
+    "url": "/React-Employee-Directory/static/js/runtime-main.a3474009.js"
+  }
+]);
